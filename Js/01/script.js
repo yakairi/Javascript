a8953752534aa545452a3b978590fcf29fccb23d
yakairi <yakairigonzalez@gmail.com>
2023-06-07 20:52:56 -0300

    datos proimitivos

--- Js/01/script.js
+++ Js/01/script.js
@@ -5,4 +5,21 @@ let apellido = 'Gonzalez'
 const mensaje = 'Mensaje de prueba'
 //const no  puede cambiar su valor. 
 console.log(apellido)
-//
+//Tipos de datos
+// datos primitivos
+
+let uno
+let dos = '1'
+console.log(uno)
+///////////////////////
+let name = 'yaki'
+let lastname ='Gonzalez'
+let age ='39'
+alert(`hola` + name)
+alert(`Hola: ${lastname}`)
+
+let primernum = prompt('Ingrese su nuemro')
+let segundonum = prompt('Ingrese su nuemro')
+let resultado = Number(primernum) + Number(segundonum)
+
+alert(`El resultado es ${resultado}`)
